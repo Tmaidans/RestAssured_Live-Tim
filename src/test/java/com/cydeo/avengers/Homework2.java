92ad3d44c005eb78024bb6f9ab3d89956ea4df65
mehmetCt <mehmet@cybertekschool.com>
2023-08-21 13:08:38 -0400

    avengers - week1

---
+++ src/test/java/com/cydeo/avengers/Homework2.java
@@ -0,0 +1,22 @@
+package com.cydeo.avengers;
+
+import org.junit.jupiter.api.Test;
+
+public class Homework2 {
+
+    /**
+     * TASK 2 :
+     * - Given accept type is Json
+     * - Query param value - q={"department_id":80}
+     * - When users sends request to /employees
+     * - Then status code is 200
+     * - And Content - Type is Json
+     * - And all job_ids start with 'SA'
+     * - And all department_ids are 80
+     * - Count is 25
+     */
+    @Test
+    public void task2() {
+
+    }
+}
